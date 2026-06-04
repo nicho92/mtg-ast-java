3d1f1599ae14ca444624ebd8e8900cd80ec7e10a
Nicho <nicolas.pihen@gmail.com>
2026-06-04 11:55:52 +0200

    Add files via upload

---
+++ src/main/java/org/magic/api/ast/abilities/parsers/SagaAbilityParser.java
@@ -0,0 +1,48 @@
+package org.magic.api.ast.abilities.parsers;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.magic.api.ast.abilities.AbilityNode;
+import org.magic.api.ast.abilities.SagaAbility;
+import org.magic.api.ast.factories.EffectFactory;
+import org.magic.api.ast.interfaces.AbilityParser;
+
+public class SagaAbilityParser implements AbilityParser {
+
+	private static final Pattern PATTERN = Pattern.compile("^(I|II|III|IV|V|VI)\\s+[—-]\\s+(.*)$", Pattern.CASE_INSENSITIVE);
+
+	@Override
+	public boolean supports(String text) {
+		return PATTERN.matcher(text).matches();
+	}
+
+	@Override
+	public AbilityNode parse(String text) {
+
+		var matcher = PATTERN.matcher(text);
+
+		matcher.find();
+
+		var triggerText = matcher.group(2);
+
+		return new SagaAbility(romanToInt(matcher.group(1)), List.of(new EffectFactory().parse(triggerText)));
+	}
+	
+	private int romanToInt(String roman) {
+
+	    return switch (roman) {
+
+	        case "I" -> 1;
+	        case "II" -> 2;
+	        case "III" -> 3;
+	        case "IV" -> 4;
+	        case "V" -> 5;
+	        case "VI" -> 6;
+
+	        default ->
+	                throw new IllegalArgumentException(
+	                        roman);
+	    };
+	}
+}
