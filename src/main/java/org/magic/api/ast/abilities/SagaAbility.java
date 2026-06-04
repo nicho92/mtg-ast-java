d637392d737e6c896c8bc958960594a3bb612dad
Nicho <nicolas.pihen@gmail.com>
2026-06-04 11:56:30 +0200

    Add files via upload

---
+++ src/main/java/org/magic/api/ast/abilities/SagaAbility.java
@@ -0,0 +1,9 @@
+package org.magic.api.ast.abilities;
+
+import java.util.List;
+
+import org.magic.api.ast.effects.EffectNode;
+
+public record SagaAbility(int num, List<EffectNode> effects )implements AbilityNode {
+
+}
