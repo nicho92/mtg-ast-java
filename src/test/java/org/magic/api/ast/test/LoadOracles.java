9731d1c226cd0b30cff045404c6314d904e01ef9
nicol <nicol@host.docker.internal>
2026-06-05 10:10:27 +0200

    updates

--- src/test/java/org/magic/api/ast/test/LoadOracles.java
+++ src/test/java/org/magic/api/ast/test/LoadOracles.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.magic.api.ast.engine.OracleParser;
 
-public class LoadOracles {
+class LoadOracles {
 
 	
 	
@@ -40,36 +40,5 @@ public class LoadOracles {
 			
 			System.out.println("=========");
 		}
-	}
-	
-	@Test
-	void testPlaneswalkerAbilities() {
-		var parser = new OracleParser();
-		var oracleText = """
-				+1: Draw a card.
-				0: Discard a card.
-				−3: Destroy target creature.
-				-X: Draw X cards.
-				""";
-		var ast = parser.parse("Liliana", oracleText);
-		var abilities = ast.getAbilities();
-		
-		org.junit.jupiter.api.Assertions.assertEquals(4, abilities.size());
-		
-		org.junit.jupiter.api.Assertions.assertInstanceOf(org.magic.api.ast.abilities.PlaneswalkerAbility.class, abilities.get(0));
-		var a0 = (org.magic.api.ast.abilities.PlaneswalkerAbility) abilities.get(0);
-		org.junit.jupiter.api.Assertions.assertEquals("+1", a0.loyalty());
-		
-		org.junit.jupiter.api.Assertions.assertInstanceOf(org.magic.api.ast.abilities.PlaneswalkerAbility.class, abilities.get(1));
-		var a1 = (org.magic.api.ast.abilities.PlaneswalkerAbility) abilities.get(1);
-		org.junit.jupiter.api.Assertions.assertEquals("0", a1.loyalty());
-		
-		org.junit.jupiter.api.Assertions.assertInstanceOf(org.magic.api.ast.abilities.PlaneswalkerAbility.class, abilities.get(2));
-		var a2 = (org.magic.api.ast.abilities.PlaneswalkerAbility) abilities.get(2);
-		org.junit.jupiter.api.Assertions.assertEquals("−3", a2.loyalty());
-		
-		org.junit.jupiter.api.Assertions.assertInstanceOf(org.magic.api.ast.abilities.PlaneswalkerAbility.class, abilities.get(3));
-		var a3 = (org.magic.api.ast.abilities.PlaneswalkerAbility) abilities.get(3);
-		org.junit.jupiter.api.Assertions.assertEquals("-X", a3.loyalty());
 	}
 }
