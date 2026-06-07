2381e7bc8cae1e29764c5a838e478ad986efd0fb
Nicho <nicolas.pihen@gmail.com>
2026-06-07 10:30:56 +0200

    feat: implement accept method in KeywordGroupAbility for Visitor pattern

--- src/main/java/org/magic/api/ast/abilities/KeywordGroupAbility.java
+++ src/main/java/org/magic/api/ast/abilities/KeywordGroupAbility.java
@@ -2,6 +2,12 @@ package org.magic.api.ast.abilities;
 
 import java.util.List;
 
-public record KeywordGroupAbility(List<KeywordAbility> keywords) implements AbilityNode {
+import org.magic.api.ast.abilities.visitor.AbilityVisitor;
 
+public record KeywordGroupAbility(List<KeywordAbility> keywords) implements AbilityNode {
+	
+	@Override
+	public <T> T accept(AbilityVisitor<T> visitor) {
+		return visitor.visit(this);
+	}
 }
