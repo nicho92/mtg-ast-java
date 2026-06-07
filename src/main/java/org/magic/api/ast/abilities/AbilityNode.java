ce6942b278a80de8e606ebd8c9df87fc02746414
Nicho <nicolas.pihen@gmail.com>
2026-06-07 10:30:31 +0200

    feat: add accept method to AbilityNode interface for Visitor pattern

--- src/main/java/org/magic/api/ast/abilities/AbilityNode.java
+++ src/main/java/org/magic/api/ast/abilities/AbilityNode.java
@@ -1,10 +1,20 @@
 package org.magic.api.ast.abilities;
 
+import org.magic.api.ast.abilities.visitor.AbilityVisitor;
+
+/**
+ * Base interface for all ability nodes in the MTG AST.
+ * Implements the Visitor pattern to support polymorphic operations.
+ */
 public interface AbilityNode {
-
 	
-	
-	
-	
-
+	/**
+	 * Accepts a visitor to process this ability node.
+	 * This method implements the Visitor design pattern.
+	 * 
+	 * @param <T> the return type of the visitor
+	 * @param visitor the visitor to accept
+	 * @return the result of the visitor's processing
+	 */
+	<T> T accept(AbilityVisitor<T> visitor);
 }
