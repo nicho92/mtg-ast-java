1fd2263deb25b1a288d08114da4a37d3767a0dc7
Nicho <nicolas.pihen@gmail.com>
2026-06-07 10:30:43 +0200

    feat: implement accept method in KeywordAbility for Visitor pattern

--- src/main/java/org/magic/api/ast/abilities/KeywordAbility.java
+++ src/main/java/org/magic/api/ast/abilities/KeywordAbility.java
@@ -1,6 +1,11 @@
 package org.magic.api.ast.abilities;
 
-public record KeywordAbility(String keyword, String parameter) implements AbilityNode 
-{
+import org.magic.api.ast.abilities.visitor.AbilityVisitor;
 
+public record KeywordAbility(String keyword, String parameter) implements AbilityNode {
+	
+	@Override
+	public <T> T accept(AbilityVisitor<T> visitor) {
+		return visitor.visit(this);
+	}
 }
