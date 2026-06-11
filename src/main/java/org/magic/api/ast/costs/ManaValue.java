5a9d9841a041e58c13b5a4c056edd4d1b6616a56
nicol <nicol@host.docker.internal>
2026-06-11 14:13:53 +0200

    Merge branch 'master' of https://github.com/nicho92/mtg-ast-java.git

--- src/main/java/org/magic/api/ast/costs/ManaValue.java
+++ src/main/java/org/magic/api/ast/costs/ManaValue.java
@@ -7,9 +7,4 @@ import org.magic.api.ast.interfaces.CostNode;
 
 public record ManaValue(List<ManaSymbol> mana) implements CostNode { 
 	
-	@Override
-	public final String toString() {
-		return mana.toString();
-	}
-	
 }
